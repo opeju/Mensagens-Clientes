44d7eef166d2bf365e977cc4e692d731675ade5c
Thiago Pejú <araujo.tdesign@gmail.com>
2025-07-15 17:05:53 -0300

    Corrige uso do banco: agora PostgreSQL via DATABASE_URL

--- jericar-fullstack/src/main.py
+++ jericar-fullstack/src/main.py
@@ -10,12 +10,11 @@ from src.routes.user import user_bp
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 
 app.register_blueprint(user_bp, url_prefix='/api')
 
 # uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
